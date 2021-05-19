3daa77307f138fd77155cf566d2b679c9d062eab
cuyocosmico <cuyocosmico@gmail.com>
2021-05-18 19:21:34 -0500

    ultimo

--- app/src/main/java/net/ivanvega/audiolibros2020/MainActivity.java
+++ app/src/main/java/net/ivanvega/audiolibros2020/MainActivity.java
@@ -1,5 +1,6 @@
 package net.ivanvega.audiolibros2020;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,10 +8,18 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +27,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        //AQUI REVISO SI LE MANDARON PARAMETROS A ESTA ACTIVIDAD
+        //EN ESPECIAL REVISO SI LA ACTIVIDAD LA INVOCO LA NOTIFICACION LANZADA POR EL SERVICIO DE PRIMER PLANO
+        //
+        if(getIntent().getExtras()!=null){
+            Log.d("MSPPN", getIntent().getExtras().getString("rep","no se encontro parametro"));
+        }
+
+
         SelectorFragment selectorFragment
                 = new SelectorFragment();
 
         if ( findViewById(R.id.contenedor_pequeno) != null    &&
-            getSupportFragmentManager().findFragmentById(R.id.contenedor_pequeno) == null
+                getSupportFragmentManager().findFragmentById(R.id.contenedor_pequeno) == null
         ){
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contenedor_pequeno,
@@ -39,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
             DetalleFragment fragment =
                     (DetalleFragment)
-                    fragmentManager.findFragmentById(R.id.detalle_fragment);
+                            fragmentManager.findFragmentById(R.id.detalle_fragment);
 
             fragment.ponInfoLibro(index);
 
@@ -57,9 +74,11 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
             fragmentManager.beginTransaction().replace(R.id.contenedor_pequeno
-            , detalleFragment).addToBackStack(null).commit();
+                    , detalleFragment).addToBackStack(null).commit();
 
         }
 
     }
+
+
 }
--- app/src/main/java/net/ivanvega/audiolibros2020/services/MiServicio.java
+++ app/src/main/java/net/ivanvega/audiolibros2020/services/MiServicio.java
@@ -1,31 +1,98 @@
 package net.ivanvega.audiolibros2020.services;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import net.ivanvega.audiolibros2020.MainActivity;
+import net.ivanvega.audiolibros2020.R;
+
+import java.util.Random;
 
 public class MiServicio extends Service {
+    // Binder given to clients
+    private final IBinder binder = new MiServicioBinder();
+    // Random number generator
+    private final Random mGenerator = new Random();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class MiServicioBinder extends Binder {
+
+        public MiServicio getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return MiServicio.this;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    /** method for clients */
+    public int getRandomNumber() {
+        return mGenerator.nextInt(100);
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
         Log.d("MSAL", "servicio creado");
+
     }
+
+    private String CHANNEL_ID="CANALID";
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //Este método se manda llamar cuando invocas el servicio con startService()
         //tarea pesado debe ir en un subproceso y desencadenarse asquí
+
+        //startForeground(10001, new Notication.builder()   );
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel();
+            foregroundService();
+        }
+
         Log.d("MSAL", "Iniciando la tarea pesada ");
         try {
             Thread.sleep(5000);
 
-                AsyncTask<Integer,
-                        Integer, Boolean> task = new AsyncTask<Integer, Integer, Boolean>() {
+            AsyncTask<Integer,
+                    Integer, Boolean> task = new AsyncTask<Integer, Integer, Boolean>() {
                 @Override
                 protected void onPreExecute() {
                     super.onPreExecute();
@@ -63,7 +130,7 @@ public class MiServicio extends Service {
             };
 
             task.execute(1,2,3,4);
-            
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -73,11 +140,32 @@ public class MiServicio extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void foregroundService() {
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.putExtra("rep", "Servicio Primer plano");
+
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 1002, notificationIntent, 0);
+
+        Notification notification =
+                new Notification.Builder(this, CHANNEL_ID)
+                        .setContentTitle("Titulo")
+                        .setContentText("Servicio en ejecucion")
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentIntent(pendingIntent)
+                        .setTicker("Se inicio el servicio")
+                        .build();
+
+        // Notification ID cannot be 0.
+        startForeground(1000, notification);
+
     }
+
+
 
     @Override
     public void onDestroy() {
